2d592fd717a56fcd4c3027a119b489e3b8b096f1
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-04-25 13:12:05 -0400

    OS Module

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const path = require('path');
+const os = require('os');
 
-var pathObj = path.parse(__filename);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 
-console.log(pathObj);
+console.log('Total Memory:'+ totalMemory);
+
+//Template String
+//ES6 /  ES2015
+
+console.log(`Total Memory:${totalMemory}`);
+console.log(`Free Memory:${freeMemory}`);
